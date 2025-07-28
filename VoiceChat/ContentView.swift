b5168bba3883aeadb6e27a53b511a31e6bbc66ae
Erik Flores <erikfloresq@icloud.com>
2025-07-28 10:51:24 -0500

    Initial Commit

---
+++ VoiceChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceChat
+//
+//  Created by Erik Flores on 28/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceChat/VoiceChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceChatApp.swift
+//  VoiceChat
+//
+//  Created by Erik Flores on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
